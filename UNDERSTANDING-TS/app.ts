097532026693456d64b58873ff0e0c77617bb1c6
Tatsuro <contacts@tatsuro.dev>
2020-07-10 05:59:40 +0900

    javascriptの型とtypescriptの型の違い 11

--- UNDERSTANDING-TS/app.ts
+++ UNDERSTANDING-TS/app.ts
@@ -1,11 +1,14 @@
 function add(n1: number, n2: number) {
+  // typescriptではなく、javascriptで型チェックを行う時はtypeof演算子を使用
+  // typeofだとjavascriptの実行時にerrorに気づくことになるが、typescriptだと開発時にエラーを発見できる。また、typescriptが役に立つのは開発時のみ、実行時(runtime)にはcompileされたjavascriptが仕事を行うのでtypescriptは何もしない点に注意
+  // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+  //   throw new Error('入力値が正しくありません');
+  // }
   return n1 + n2;
 }
 
 const number1 = 5;
-// const number1 = '5';
 const number2 = 2.8;
 
-// typescriptはerrorがある時もdefaultではjavascript fileをcompileする
 const result = add(number1, number2);
 console.log(result);
