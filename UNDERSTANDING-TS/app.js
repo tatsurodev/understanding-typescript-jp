60acc6097d45fbf51b1bbc717ab263a1fe04404c
Tatsuro <contacts@tatsuro.dev>
2020-07-10 08:00:53 +0900

    tuple型 18

--- UNDERSTANDING-TS/app.js
+++ UNDERSTANDING-TS/app.js
@@ -7,11 +7,23 @@
 //   age: 30,
 // };
 // 型推論
+// roleの型をtupleにしたい時、role: [2, 'author'];だとtupleと推論されないのでobject全体のpropertyの型を明示させる必要あり
 var person = {
     name: 'yota',
     age: 30,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
 };
+// ok
+// tupleは、値をpushで追加できてしまうので注意。下記はerrorが発生しない
+// person.role.push('admin');
+// error
+// role: [number, string];と型を指定しているのでerror
+// person.role[1] = 10;
+// 空の配列の代入はerror
+// person.role = [];
+// 3つ目の要素があるのでerror
+// person.role = [0, 'admin', 'user'];
 var favoriteActivities;
 favoriteActivities = ['Sports'];
 console.log(person.name);
--- UNDERSTANDING-TS/app.ts
+++ UNDERSTANDING-TS/app.ts
@@ -8,11 +8,30 @@
 // };
 
 // 型推論
-const person = {
+// roleの型をtupleにしたい時、role: [2, 'author'];だとtupleと推論されないのでobject全体のpropertyの型を明示させる必要あり
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: 'yota',
   age: 30,
-  hobbies: ['Sports', 'Cooking']
+  hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author'],
 };
+
+// ok
+// tupleは、値をpushで追加できてしまうので注意。下記はerrorが発生しない
+// person.role.push('admin');
+
+// error
+// role: [number, string];と型を指定しているのでerror
+// person.role[1] = 10;
+// 空の配列の代入はerror
+// person.role = [];
+// 3つ目の要素があるのでerror
+// person.role = [0, 'admin', 'user'];
 
 let favoriteActivities: string[];
 favoriteActivities = ['Sports'];
