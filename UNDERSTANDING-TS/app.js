cadf1729d6ff2e92a7c794ab47cafd36eedffa3f
Tatsuro <contacts@tatsuro.dev>
2020-07-10 06:21:52 +0900

    number, string, boolean型の使用 13

--- UNDERSTANDING-TS/app.js
+++ UNDERSTANDING-TS/app.js
@@ -1,12 +1,19 @@
-function add(n1, n2) {
+function add(n1, n2, showResult, phrase) {
     // typescriptではなく、javascriptで型チェックを行う時はtypeof演算子を使用
     // typeofだとjavascriptの実行時にerrorに気づくことになるが、typescriptだと開発時にエラーを発見できる。また、typescriptが役に立つのは開発時のみ、実行時(runtime)にはcompileされたjavascriptが仕事を行うのでtypescriptは何もしない点に注意
     // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
     //   throw new Error('入力値が正しくありません');
     // }
-    return n1 + n2;
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return result;
+    }
 }
 var number1 = 5;
 var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
+var printResult = true;
+var resultPhrase = 'Result: ';
+add(number1, number2, printResult, resultPhrase);
--- UNDERSTANDING-TS/app.ts
+++ UNDERSTANDING-TS/app.ts
@@ -1,14 +1,20 @@
-function add(n1: number, n2: number) {
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
   // typescriptではなく、javascriptで型チェックを行う時はtypeof演算子を使用
   // typeofだとjavascriptの実行時にerrorに気づくことになるが、typescriptだと開発時にエラーを発見できる。また、typescriptが役に立つのは開発時のみ、実行時(runtime)にはcompileされたjavascriptが仕事を行うのでtypescriptは何もしない点に注意
   // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
   //   throw new Error('入力値が正しくありません');
   // }
-  return n1 + n2;
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return result;
+  }
 }
 
 const number1 = 5;
 const number2 = 2.8;
+const printResult = true;
+const resultPhrase = 'Result: '
 
-const result = add(number1, number2);
-console.log(result);
+add(number1, number2, printResult, resultPhrase);
