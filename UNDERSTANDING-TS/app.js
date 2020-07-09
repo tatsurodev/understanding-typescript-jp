38bb297ab9c18a798826ff6e42dcaf9539da0feb
Tatsuro <contacts@tatsuro.dev>
2020-07-10 08:37:19 +0900

    enum型 19

--- UNDERSTANDING-TS/app.js
+++ UNDERSTANDING-TS/app.js
@@ -8,12 +8,17 @@
 // };
 // 型推論
 // roleの型をtupleにしたい時、role: [2, 'author'];だとtupleと推論されないのでobject全体のpropertyの型を明示させる必要あり
-var person = {
-    name: 'yota',
-    age: 30,
-    hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author']
-};
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: 'yota',
+//   age: 30,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author'],
+// };
 // ok
 // tupleは、値をpushで追加できてしまうので注意。下記はerrorが発生しない
 // person.role.push('admin');
@@ -24,6 +29,22 @@ var person = {
 // person.role = [];
 // 3つ目の要素があるのでerror
 // person.role = [0, 'admin', 'user'];
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+// enumで人間が理解しやすい定数に数値を割り当てられる。文字列も可
+var Role;
+(function (Role) {
+    Role["ADMIN"] = "ADMIN";
+    Role[Role["READ_ONLY"] = 100] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 200] = "AUTHOR";
+})(Role || (Role = {}));
+var person = {
+    name: 'yota',
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    role: Role.ADMIN
+};
 var favoriteActivities;
 favoriteActivities = ['Sports'];
 console.log(person.name);
@@ -31,4 +52,7 @@ for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
     // console.log(hobby.map());
+}
+if (person.role === Role.ADMIN) {
+    console.log('管理者ユーザ');
 }
--- UNDERSTANDING-TS/app.ts
+++ UNDERSTANDING-TS/app.ts
@@ -9,17 +9,17 @@
 
 // 型推論
 // roleの型をtupleにしたい時、role: [2, 'author'];だとtupleと推論されないのでobject全体のpropertyの型を明示させる必要あり
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
-  name: 'yota',
-  age: 30,
-  hobbies: ['Sports', 'Cooking'],
-  role: [2, 'author'],
-};
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: 'yota',
+//   age: 30,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author'],
+// };
 
 // ok
 // tupleは、値をpushで追加できてしまうので注意。下記はerrorが発生しない
@@ -33,6 +33,24 @@ const person: {
 // 3つ目の要素があるのでerror
 // person.role = [0, 'admin', 'user'];
 
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+// enumで人間が理解しやすい定数に数値を割り当てられる。文字列も可
+enum Role {
+  ADMIN = 'ADMIN',
+  READ_ONLY = 100,
+  AUTHOR = 200,
+}
+
+const person = {
+  name: 'yota',
+  age: 30,
+  hobbies: ['Sports', 'Cooking'],
+  role: Role.ADMIN,
+};
+
 let favoriteActivities: string[];
 favoriteActivities = ['Sports'];
 
@@ -41,4 +59,8 @@ console.log(person.name);
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
   // console.log(hobby.map());
+}
+
+if (person.role === Role.ADMIN) {
+  console.log('管理者ユーザ');
 }
