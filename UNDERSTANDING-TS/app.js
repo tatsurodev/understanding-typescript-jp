9ed29db3152722e47ac0a5d84f96b99620fe441d
Tatsuro <contacts@tatsuro.dev>
2020-07-10 22:54:20 +0900

    unknown型 28

--- UNDERSTANDING-TS/app.js
+++ UNDERSTANDING-TS/app.js
@@ -1,30 +1,11 @@
-function add(n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Max';
+// ok: any->string, error: unknown->string
+// userName = userInput;
+// unknown型は何かをする時に型チェックが必要になるのでanyよりはいい選択肢
+// ok: unknownのtypeがstring->string
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-// 関数の返り値、voidとundefinedの違い
-// 返り値がvoidの時undefinedを返すが、returnは不要
-function printResult(num) {
-    console.log('Result: ' + num);
-}
-// console.log(printResult(add(5, 12)));
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-addAndHandle(10, 20, function (result) {
-    console.log(result);
-    // 返り値にvoidが指定されている時でもreturnしてもerrorは発生しない
-    return result;
-});
-// 返り値にundefinedを指定すると、returnが必須。通常、関数の返り値にundefinedは避けるべき
-// function printResult(num: number): undefined {
-//   console.log('Result: ' + num);
-//   return;
-// }
-// functionを代入する変数にfunction型を与え、代入できる関数を制限する
-var combineValues;
-combineValues = add;
-// combineValues = 5;
-// combineValues = printResult;
-console.log(combineValues(8, 8));
-printResult(add(5, 12));
--- UNDERSTANDING-TS/app.ts
+++ UNDERSTANDING-TS/app.ts
@@ -1,36 +1,14 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = 'Max';
+
+// ok: any->string, error: unknown->string
+// userName = userInput;
+
+// unknown型は何かをする時に型チェックが必要になるのでanyよりはいい選択肢
+// ok: unknownのtypeがstring->string
+if (typeof userInput === 'string') {
+  userName = userInput;
 }
-
-// 関数の返り値、voidとundefinedの違い
-// 返り値がvoidの時undefinedを返すが、returnは不要
-function printResult(num: number): void {
-  console.log('Result: ' + num);
-}
-// console.log(printResult(add(5, 12)));
-
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-  const result = n1 + n2;
-  cb(result);
-}
-
-addAndHandle(10, 20, (result) => {
-  console.log(result);
-  // 返り値にvoidが指定されている時でもreturnしてもerrorは発生しない
-  return result;
-})
-
-// 返り値にundefinedを指定すると、returnが必須。通常、関数の返り値にundefinedは避けるべき
-// function printResult(num: number): undefined {
-//   console.log('Result: ' + num);
-//   return;
-// }
-
-// functionを代入する変数にfunction型を与え、代入できる関数を制限する
-let combineValues: (a: number, b: number) => number;
-combineValues = add;
-// combineValues = 5;
-// combineValues = printResult;
-console.log(combineValues(8, 8));
-
-printResult(add(5, 12));
