bee7eeaece827a497dd9181a658ede34bf64b905
Tatsuro <contacts@tatsuro.dev>
2020-07-11 01:56:03 +0900

    tsconfigのlib設定 37

--- UNDERSTANDING-TS/app.js
+++ UNDERSTANDING-TS/app.js
@@ -1,5 +1,5 @@
 "use strict";
-let age;
-age = 30;
-const userName = 'Maximilian';
-console.log(userName);
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    console.log('Clicked!');
+});
