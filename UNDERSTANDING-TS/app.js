b0e48f00e2c1b4d4ee7a4dfbd8737ccff7a59684
Tatsuro <contacts@tatsuro.dev>
2020-07-10 10:30:09 +0900

    functionの返り値void, undefined 25

--- UNDERSTANDING-TS/app.js
+++ UNDERSTANDING-TS/app.js
@@ -1,23 +1,15 @@
-function combine(input1, input2, 
-// literal型かつunion型
-resultConversion) {
-    var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-        result = +input1 + +input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
-    // if (resultConversion === 'as-number') {
-    //   return +result;
-    // } else {
-    //   return result.toString();
-    // }
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
-var combinedStringAges = combine('30', '26', 'as-number');
-console.log(combinedStringAges);
-var combinedNames = combine('Max', 'Anna', 'as-text');
-console.log(combinedNames);
+// 関数の返り値、voidとundefinedの違い
+// 返り値がvoidの時undefinedを返すが、returnは不要
+function printResult(num) {
+    console.log('Result: ' + num);
+}
+// console.log(printResult(add(5, 12)));
+// 返り値にundefinedを指定すると、returnが必須。通常、関数の返り値にundefinedは避けるべき
+// function printResult(num: number): undefined {
+//   console.log('Result: ' + num);
+//   return;
+// }
+printResult(add(5, 12));
--- UNDERSTANDING-TS/app.ts
+++ UNDERSTANDING-TS/app.ts
@@ -1,32 +1,18 @@
-// type aliasの使用で、型の再利用、型名を説明的に、また型の定義を一箇所に集中して管理することができる
-type Combinable = number | string;
-type ConversionDescriptor = 'as-number' | 'as-text';
-
-function combine(
-  input1: Combinable,
-  input2: Combinable,
-  // literal型かつunion型
-  resultConversion: ConversionDescriptor,
-) {
-  let result;
-  if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  return result;
-  // if (resultConversion === 'as-number') {
-  //   return +result;
-  // } else {
-  //   return result.toString();
-  // }
+function add(n1: number, n2: number) {
+  return n1 + n2;
 }
 
-const combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
+// 関数の返り値、voidとundefinedの違い
+// 返り値がvoidの時undefinedを返すが、returnは不要
+function printResult(num: number): void {
+  console.log('Result: ' + num);
+}
+// console.log(printResult(add(5, 12)));
 
-const combinedStringAges = combine('30', '26', 'as-number');
-console.log(combinedStringAges);
+// 返り値にundefinedを指定すると、returnが必須。通常、関数の返り値にundefinedは避けるべき
+// function printResult(num: number): undefined {
+//   console.log('Result: ' + num);
+//   return;
+// }
 
-const combinedNames = combine('Max', 'Anna', 'as-text');
-console.log(combinedNames);
+printResult(add(5, 12));
