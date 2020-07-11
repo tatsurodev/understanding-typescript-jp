020f9acf53317786b8792a3a981e1f083795e47f
Tatsuro <contacts@tatsuro.dev>
2020-07-11 15:03:53 +0900

    section5の雛形 58

--- UNDERSTANDING-TS/dist/app.js
+++ UNDERSTANDING-TS/dist/app.js
@@ -1,27 +1,9 @@
 "use strict";
-const button = document.querySelector('button');
-if (button) {
-    button.addEventListener('click', event => {
-        console.log(event);
-    });
+class Department {
+    constructor(n) {
+        this.name = n;
+    }
 }
-const hobbies = ['Sports', 'Cooking'];
-const activeHobbies = ['Hiking', ...hobbies];
-activeHobbies.push(...hobbies);
-const person = {
-    firstName: 'Max',
-    age: 30,
-};
-const copiedPerson = Object.assign({}, person);
-const add = (...numbers) => {
-    return numbers.reduce((curResult, curValue) => {
-        return curResult + curValue;
-    }, 0);
-};
-const addedNumbers = add(5, 10, 2, 3.7);
-console.log(addedNumbers);
-const [hobby1, hobby2, ...remainingHobbies] = hobbies;
-console.log(hobbies, hobby1, hobby2);
-const { firstName: userName, age } = person;
-console.log(userName, age, person);
+const accounting = new Department('Accounting');
+console.log(accounting);
 //# sourceMappingURL=app.js.map
