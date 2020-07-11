772277e43433f161755baf629331c93a2b489ea9
Tatsuro <contacts@tatsuro.dev>
2020-07-12 01:57:29 +0900

    private, public修飾子 61

--- UNDERSTANDING-TS/src/app.ts
+++ UNDERSTANDING-TS/src/app.ts
@@ -1,5 +1,7 @@
 class Department {
   name: string;
+  private employees: string[] = [];
+
   constructor(n: string) {
     this.name = n;
   }
@@ -7,11 +9,28 @@ class Department {
   describe(this: Department) {
     console.log('Department: ' + this.name);
   }
+  addEmployee(employee: string) {
+    // validation系の処理も追加できるので、下記のaccounting.employees[2]='Anna';のように直接外部からaccessして変更できるような仕様は良くない。employeesをprivateにしてこのmethodを通じてのみ変更できるようにする
+    this.employees.push(employee);
+  }
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Department('Accounting');
-accounting.describe();
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
 
+// accounting.employees[2] = 'Anna';
+accounting.name = 'NEW NAME';
+
+accounting.describe();
+accounting.printEmployeeInformation();
+
+/*
 const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
 // accountingCopyにname propertyがない時にaccountingCopy.describe()すると、describe methodでthisの型をDepartmentにしているのでerrorが出る
 accountingCopy.describe();
+*/
