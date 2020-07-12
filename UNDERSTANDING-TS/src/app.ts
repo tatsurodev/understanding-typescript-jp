973e46dcb8b23969f08b8b8b3d1ea4691703bbe8
Tatsuro <contacts@tatsuro.dev>
2020-07-12 23:49:32 +0900

    classでinterfaceを実装 72

--- UNDERSTANDING-TS/src/app.ts
+++ UNDERSTANDING-TS/src/app.ts
@@ -1,18 +1,25 @@
-interface Person {
+// interfaceは具体的な実装を含まない、一方抽象classは具体的な実装と抽象的な実装の両方を含めることができる
+interface Greetable {
   name: string;
-  age: number;
 
   greet(phrase: string): void;
 }
 
-let user1: Person;
+// type Person = {};
 
-user1 = {
-  name: 'Max',
-  age: 30,
+class Person implements Greetable {
+  name: string;
+  age = 30;
+
+  constructor(n: string) {
+    this.name = n;
+  }
   greet(phrase: string) {
     console.log(phrase + ' ' + this.name);
   }
 }
 
+let user1: Greetable;
+user1 = new Person('Max');
 user1.greet('Hello I am');
+console.log(user1);
