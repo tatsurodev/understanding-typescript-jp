22172cb06be64763acb19ceac0b602c9aad05553
Tatsuro <contacts@tatsuro.dev>
2020-07-14 15:11:23 +0900

    genericsに制約を加える 95

--- UNDERSTANDING-TS/src/app.ts
+++ UNDERSTANDING-TS/src/app.ts
@@ -15,7 +15,7 @@ promise.then(data => {
 */
 
 // 返り値の型は、2つの引数の交差型であることは推論できているので、その引数に型T, Uを与えるとその2つの交差型T&Uが返り値として認識される
-function merge<T, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 // fail 返り値はobject&objectなので只のobjectとして認識されてしまう
@@ -27,4 +27,4 @@ function merge<T, U>(objA: T, objB: U) {
 const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
 // genericsの型を推論させるのではなく明示させることもできるが冗長なでの上の引数の方から返り値も推論させるのがbetter
 // const mergedObj = merge<{ name: string; hobbies: string[] }, { age: number }>({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
-console.log(mergedObj.age);
+console.log(mergedObj);
