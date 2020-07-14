26ef425487289fa79fdf2b18a93f11d02bfc6476
Tatsuro <contacts@tatsuro.dev>
2020-07-14 09:41:33 +0900

    関数overload 87

--- UNDERSTANDING-TS/src/app.ts
+++ UNDERSTANDING-TS/src/app.ts
@@ -39,6 +39,11 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 type Universal = Combinable & Numeric;
 
+// 関数overload, 戻り値の型を正しく推論できない時に便利
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
+function add(a: string, b: number): string;
+function add(a: number, b: string): string;
 // type guard
 function add(a: Combinable, b: Combinable) {
   // typeof演算子でtype guard
@@ -47,105 +52,110 @@ function add(a: Combinable, b: Combinable) {
   }
   return a + b;
 }
+// 上記の関数overloadがないと返り値がCombinableになってしまい、result.splitが正しく認識できない
+const result = add('Hello', 'TypeScript');
+result.split(' ');
 
-type UnknownEmployee = Employee | Admin;
-function printEmployeeInformation(emp: UnknownEmployee) {
-  console.log(emp.name);
-  // typeof empとしてもobjectとしかならないのでtype guardにならない。in演算子でobjectの中にあるpropertyがあるかどうかでtype guardを行う
-  if ('privileges' in emp) {
-    console.log('Privileges: ' + emp.privileges);
-  }
+// type UnknownEmployee = Employee | Admin;
+// function printEmployeeInformation(emp: UnknownEmployee) {
+//   console.log(emp.name);
+//   // typeof empとしてもobjectとしかならないのでtype guardにならない。in演算子でobjectの中にあるpropertyがあるかどうかでtype guardを行う
+//   if ('privileges' in emp) {
+//     console.log('Privileges: ' + emp.privileges);
+//   }
 
-  if ('startDate' in emp) {
-    console.log('Start Date: ' + emp.startDate);
-  }
-}
+//   if ('startDate' in emp) {
+//     console.log('Start Date: ' + emp.startDate);
+//   }
+// }
 
-printEmployeeInformation({ name: 'Manu', startDate: new Date() });
+// printEmployeeInformation({ name: 'Manu', startDate: new Date() });
 
-class Car {
-  drive() {
-    console.log('運転中...');
-  }
-}
+// class Car {
+//   drive() {
+//     console.log('運転中...');
+//   }
+// }
 
-class Truck {
-  drive() {
-    console.log('トラックを運転中...');
-  }
-  loadCargo(amount: number) {
-    console.log('荷物を載せています...' + amount);
-  }
-}
+// class Truck {
+//   drive() {
+//     console.log('トラックを運転中...');
+//   }
+//   loadCargo(amount: number) {
+//     console.log('荷物を載せています...' + amount);
+//   }
+// }
 
-type Vehicle = Car | Truck;
-const v1 = new Car();
-const v2 = new Truck();
+// type Vehicle = Car | Truck;
+// const v1 = new Car();
+// const v2 = new Truck();
 
-function useVehicle(vehicle: Vehicle) {
-  vehicle.drive();
-  // classの場合、instanceofをtype guardに使用できる。interfaceで実装したobjectはinterface自体がjavascriptにcompileされないためinstanceofをtype guardに使用できない
-  if (vehicle instanceof Truck) {
-    vehicle.loadCargo(1000);
-  }
-}
+// function useVehicle(vehicle: Vehicle) {
+//   vehicle.drive();
+//   // classの場合、instanceofをtype guardに使用できる。interfaceで実装したobjectはinterface自体がjavascriptにcompileされないためinstanceofをtype guardに使用できない
+//   if (vehicle instanceof Truck) {
+//     vehicle.loadCargo(1000);
+//   }
+// }
 
-useVehicle(v1);
-useVehicle(v2);
+// useVehicle(v1);
+// useVehicle(v2);
 
-// descriminated unions, 判別可能なunion型。共通のpropertyの値によって場合分ける
-interface Bird {
-  // 共通のpropertyをセットする
-  type: 'bird';
-  flyingSpeed: number;
-}
+// // descriminated unions, 判別可能なunion型。共通のpropertyの値によって場合分ける
+// interface Bird {
+//   // 共通のpropertyをセットする
+//   type: 'bird';
+//   flyingSpeed: number;
+// }
 
-interface Horse {
-  type: 'horse';
-  runningSpeed: number;
-}
+// interface Horse {
+//   type: 'horse';
+//   runningSpeed: number;
+// }
 
-type Animal = Bird | Horse;
+// type Animal = Bird | Horse;
 
-function moveAnimal(animal: Animal) {
-  let speed;
-  switch (animal.type) {
-    case 'bird':
-      speed = animal.flyingSpeed;
-      break;
-    case 'horse':
-      speed = animal.runningSpeed;
-  }
-  console.log('移動速度: ' + speed);
-}
+// function moveAnimal(animal: Animal) {
+//   let speed;
+//   switch (animal.type) {
+//     case 'bird':
+//       speed = animal.flyingSpeed;
+//       break;
+//     case 'horse':
+//       speed = animal.runningSpeed;
+//   }
+//   console.log('移動速度: ' + speed);
+// }
 
-moveAnimal({ type: 'bird', flyingSpeed: 10 });
+// moveAnimal({ type: 'bird', flyingSpeed: 10 });
 
-// 前からtype cast, reactのjsxと衝突する可能性あり
-// const userInputElement = <HTMLInputElement>document.getElementById('user-input')!;
-// 後ろからtype cast
-const userInputElement = document.getElementById('user-input')! as HTMLInputElement;
-userInputElement.value = 'こんにちは';
-// non-null assertion operatorを使用しないversion
-/*
-type castをしようするとnullでないことを意味するためここではtype castしない
-const userInputElement = document.getElementById('user-input');
-if (userInputElement) {
-  (userInputElement as HTMLInputElement).value = 'こんにちは';
-}
-*/
+// // 前からtype cast, reactのjsxと衝突する可能性あり
+// // const userInputElement = <HTMLInputElement>document.getElementById('user-input')!;
+// // 後ろからtype cast
+// const userInputElement = document.getElementById('user-input')! as HTMLInputElement;
+// userInputElement.value = 'こんにちは';
+// // non-null assertion operatorを使用しないversion
+// /*
+// type castをしようするとnullでないことを意味するためここではtype castしない
+// const userInputElement = document.getElementById('user-input');
+// if (userInputElement) {
+//   (userInputElement as HTMLInputElement).value = 'こんにちは';
+// }
+// */
 
-// index type, index signature
-interface ErrorContainer {
-  // 分かっているpropertyを明記したい時
-  // error
-  // id: number;
-  // ok
-  // id: string;
-  [prop: string]: string;
-}
+// // index type, index signature
+// interface ErrorContainer {
+//   // 分かっているpropertyを明記したい時
+//   // error
+//   // id: number;
+//   // ok
+//   // id: string;
+//   [prop: string]: string;
+// }
 
-const errorBag: ErrorContainer = {
-  emial: 'メールアドレスではありません',
-  username: 'ユーザー名に記号を含めることはできません',
-};
+// const errorBag: ErrorContainer = {
+//   emial: 'メールアドレスではありません',
+//   username: 'ユーザー名に記号を含めることはできません',
+// };
+
+
