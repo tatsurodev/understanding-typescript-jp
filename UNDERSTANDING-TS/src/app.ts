c803d7ebc295f49ebf90f82f7fd0415d2368091e
Tatsuro <contacts@tatsuro.dev>
2020-07-16 15:07:01 +0900

    genericsとkeyofを組み合わる 97

--- UNDERSTANDING-TS/src/app.ts
+++ UNDERSTANDING-TS/src/app.ts
@@ -42,3 +42,10 @@ function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
   return [element, descriptionText];
 }
 console.log(countAndDescribe(['Sports', 'Cooking']));
+
+// keyofを使って存在しないobjectのpropertyにaccessできないように制約を加える
+function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
+  return 'Value: ' + obj[key];
+}
+
+extractAndConvert({ name: 'Max' }, 'name');
