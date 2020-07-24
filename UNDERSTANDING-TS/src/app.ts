ba2acc4ee2746063ed183ec6e08b51c0d4f38858
Tatsuro <contacts@tatsuro.dev>
2020-07-24 15:20:51 +0900

    decorator factory 105

--- UNDERSTANDING-TS/src/app.ts
+++ UNDERSTANDING-TS/src/app.ts
@@ -1,10 +1,14 @@
-// decoratorは定義時に実行される、classに対して使用するのでtargetとなるconstructorを第一引数に受け取る
-function Logger(constructor: Function) {
-  console.log('ログ出力中...');
-  console.log(constructor);
+// decorator factoryによりdecoratorをcustomizeする為に必要な引数を与えることができる
+function Logger(logString: string) {
+  // decoratorは定義時に実行される、classに対して使用するのでtargetとなるconstructorを第一引数に受け取る
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  }
 }
 
-@Logger
+// decorator factoryを実行し、中の関数部であるdecoratorが返ってくる
+@Logger('ログ出力中 - PERSON')
 class Person {
   name = 'Max';
   constructor() {
