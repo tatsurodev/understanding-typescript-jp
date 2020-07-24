a71b4fdf26642c4d6243d61f8189446196e5346c
Tatsuro <contacts@tatsuro.dev>
2020-07-24 15:23:24 +0900

    decorator, decorator factoryの実行順序 107

--- UNDERSTANDING-TS/src/app.ts
+++ UNDERSTANDING-TS/src/app.ts
@@ -1,5 +1,6 @@
 // decorator factoryによりdecoratorをcustomizeする為に必要な引数を与えることができる
 function Logger(logString: string) {
+  console.log('LOGGER ファクトリ');
   // decoratorは定義時に実行される、classに対して使用するのでtargetとなるconstructorを第一引数に受け取る
   return function (constructor: Function) {
     console.log(logString);
@@ -8,8 +9,10 @@ function Logger(logString: string) {
 }
 
 function WithTemplate(template: string, hookId: string) {
+  console.log('TEMPLATE ファクトリ');
   // constructorを使用しない時は_
   return function (constructor: any) {
+    console.log('テンプレートを表示');
     const hookEl = document.getElementById(hookId);
     const p = new constructor();
     if (hookEl) {
@@ -20,6 +23,8 @@ function WithTemplate(template: string, hookId: string) {
 }
 
 // decorator factoryを実行し、中の関数部であるdecoratorが返ってくる
+// decorator関数を返すdecorator factoryは普通に上からした、decorator関数自体は下から上へ実行される
+@Logger('ログ出力中')
 @WithTemplate('<h1>Personオブジェクト</h1>', 'app')
 class Person {
   name = 'Max';
