116aa6fef3ef90d21249b9b88d93b7e9956aebf8
Tatsuro <contacts@tatsuro.dev>
2020-07-24 15:22:30 +0900

    decoratorの使用例 106

--- UNDERSTANDING-TS/src/app.ts
+++ UNDERSTANDING-TS/src/app.ts
@@ -7,8 +7,20 @@ function Logger(logString: string) {
   }
 }
 
+function WithTemplate(template: string, hookId: string) {
+  // constructorを使用しない時は_
+  return function (constructor: any) {
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector('h1')!.textContent = p.name;
+    }
+  }
+}
+
 // decorator factoryを実行し、中の関数部であるdecoratorが返ってくる
-@Logger('ログ出力中 - PERSON')
+@WithTemplate('<h1>Personオブジェクト</h1>', 'app')
 class Person {
   name = 'Max';
   constructor() {
