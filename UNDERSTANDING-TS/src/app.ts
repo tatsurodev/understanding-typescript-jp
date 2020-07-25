16175e4117a8598a220c49d24540bc9ad512e971
Tatsuro <contacts@tatsuro.dev>
2020-07-25 14:20:30 +0900

    入力値の取得と操作 124

--- UNDERSTANDING-TS/src/app.ts
+++ UNDERSTANDING-TS/src/app.ts
@@ -19,9 +19,9 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
-  titleInptElement: HTMLInputElement;
-  descriptionInptElement: HTMLInputElement;
-  mandayInptElement: HTMLInputElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  mandayInputElement: HTMLInputElement;
 
   // constructorは基本的に要素への参照
   constructor() {
@@ -32,20 +32,53 @@ class ProjectInput {
     this.element = importedNode.firstElementChild as HTMLFormElement;
     this.element.id = 'user-input';
 
-    this.titleInptElement = this.element.querySelector('#title') as HTMLInputElement;
-    this.descriptionInptElement = this.element.querySelector('#description') as HTMLInputElement;
-    this.mandayInptElement = this.element.querySelector('#manday') as HTMLInputElement;
+    // 各入力値への参照を取得
+    this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+    this.mandayInputElement = this.element.querySelector('#manday') as HTMLInputElement;
 
     this.configure();
     this.attach();
+  }
+
+  // user inputのvalidationを行い、おｋなら取得
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredManday = this.mandayInputElement.value;
+
+    if (
+      // validation
+      enteredTitle.trim().length === 0 ||
+      enteredDescription.trim().length === 0 ||
+      enteredManday.trim().length === 0
+    ) {
+      alert('入力値が正しくありません。再度お試しください。');
+      return;
+    } else {
+      return [enteredTitle, enteredDescription, +enteredManday];
+    }
+  }
+
+  // 入力値のclear
+  private clearInputs() {
+    this.titleInputElement.value = '';
+    this.descriptionInputElement.value = '';
+    this.mandayInputElement.value = '';
   }
 
   // event handler
   @autobind
   private submitHandler(event: Event) {
     event.preventDefault();
-    console.log(this.titleInptElement.value);
-
+    const userInput = this.gatherUserInput();
+    // tupleかvoidかのcheck、tupleはjavascript上では只のarray
+    if (Array.isArray(userInput)) {
+      const [title, desc, manday] = userInput;
+      // 入力値を使ってやりたい操作
+      console.log(title, desc, manday);
+      this.clearInputs();
+    }
   }
 
   // event listenerの設定
