c612b729ccd09df3b5789342db15a5654e94281b
Tatsuro <contacts@tatsuro.dev>
2020-08-03 22:15:31 +0900

    型定義fileのないjavascript libraryに@typesで型定義fileをinstall 160

---
+++ section9-11/src/app.ts
@@ -0,0 +1,6 @@
+import { ProjectInput } from './components/project-inputs';
+import { ProjectList } from './components/project-list';
+
+new ProjectInput();
+new ProjectList('active');
+new ProjectList('finished');
