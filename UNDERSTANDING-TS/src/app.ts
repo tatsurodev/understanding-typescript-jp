2c1eb88d4d5b9666e78bcd7f4b76a2303c8743b2
Tatsuro <contacts@tatsuro.dev>
2020-08-03 22:55:05 +0900

    アンビエント宣言の利用 161

--- UNDERSTANDING-TS/src/app.ts
+++ UNDERSTANDING-TS/src/app.ts
@@ -1,3 +1,8 @@
 import _ from 'lodash';
 
 console.log(_.shuffle([1, 2, 3]));
+
+// アンビエント宣言でjavascriptから提供される関数、変数の型を宣言し、typescriptで使用できるようにする
+// declare var GLOBAL: string;
+
+// console.log(GLOBAL);
