a68c2c8db13cae30b729f2eefaeeebfa8c40854d
Tatsuro <contacts@tatsuro.dev>
2020-08-03 18:32:00 +0900

    webpack-dev-serverの設定 155

--- UNDERSTANDING-TS/webpack.config.js
+++ UNDERSTANDING-TS/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   // entryを起点としてimportされたfileを全てwebpackで処理する
   entry: './src/app.ts',
   output: {
@@ -9,6 +10,8 @@ module.exports = {
     // filename: 'bundle.[contenthash].js',
     // resolve methodで絶対パス取得
     path: path.resolve(__dirname, 'dist'),
+    // webpack dev server使用時に使う設定、webpackでbundleされたfileはmemory上に作成されるのでそのfileをindex.htmlで読み込まれるものと同じにする必要がある
+    publicPath: 'dist',
   },
   devtool: 'inline-source-map',
   // entryから取得されたfileをどのloaderで処理するか設定する
