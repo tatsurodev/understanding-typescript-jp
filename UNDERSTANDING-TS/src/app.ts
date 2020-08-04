2d29b2af7fba5c406f69b7b36f7f6bae15ac54fa
Tatsuro <contacts@tatsuro.dev>
2020-08-05 06:35:35 +0900

    formの入力値取得 168

--- UNDERSTANDING-TS/src/app.ts
+++ UNDERSTANDING-TS/src/app.ts
@@ -1,1 +1,10 @@
-// ここにコードを書きます
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+  // google apiに送信
+};
+
+form.addEventListener('submit', searchAddressHandler);
