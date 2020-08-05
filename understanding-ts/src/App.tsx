20c0a799b344a8d2d935db4fac3cf0e97cbbec8d
Tatsuro <contacts@tatsuro.dev>
2020-08-06 04:59:21 +0900

    FunctionComponentを戻り値に指定 176

--- understanding-ts/src/App.tsx
+++ understanding-ts/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
-function App() {
-    return (
-        <div className="App">
-        </div>
-    );
-}
+// function App() {
+//     return (
+//         <div className="App"></div>
+//     );
+// }
+
+// React.FCはReact.FunctionComponentと同値
+const App: React.FC = () => {
+    return <div className="App"></div>;
+};
 
 export default App;
