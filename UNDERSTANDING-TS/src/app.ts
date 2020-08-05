fddd2f3ff556d1d8bdbf6045be7487628f5c43c1
Tatsuro <contacts@tatsuro.dev>
2020-08-06 03:52:55 +0900

    google mapの代わりにOpenLayersを使う 172

--- UNDERSTANDING-TS/src/app.ts
+++ UNDERSTANDING-TS/src/app.ts
@@ -1,41 +1,26 @@
-import axios from 'axios';
 import './app.css';
 
 const form = document.querySelector('form')!;
-const addressInput = document.getElementById('address')! as HTMLInputElement;
-const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 
-type GoogleGeocodingResponse = {
-  results: { geometry: { location: { lat: number, lng: number } } }[];
-  status: 'OK' | 'ZERO_RESULTS';
-};
-
-// declare var google: any;
+declare var ol: any;
 
 function searchAddressHandler(event: Event) {
   event.preventDefault();
-  const enteredAddress = addressInput.value;
-  // google apiに送信
-  axios
-    .get<GoogleGeocodingResponse>(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_KEY}`)
-    .then(response => {
-      if (response.data.status !== 'OK') {
-        throw new Error('座標を取得できませんでした。');
-      }
-      const coordinates = response.data.results[0].geometry.location;
-      const map = new google.maps.Map(document.getElementById('map')!, {
-        center: coordinates,
-        zoom: 16,
-      });
-      new google.maps.Marker({
-        position: coordinates,
-        map,
-      });
+  // OpenLayersでlat, lngのdataで地図を表示する
+  const coordinates = { lat: 40.41, lng: -73.99 }; // Can't fetch coordinates from Google API, use dummy ones
+  document.getElementById('map')!.innerHTML = ''; // clear <p> from <div id="map">
+  new ol.Map({
+    target: 'map',
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([coordinates.lng, coordinates.lat]),
+      zoom: 16
     })
-    .catch(err => {
-      alert(err.message);
-      console.log(err);
-    });
+  });
 };
 
 form.addEventListener('submit', searchAddressHandler);
