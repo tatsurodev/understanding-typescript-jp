e821b92ad44423764aea7f401f2f8f28ff074631
Tatsuro <contacts@tatsuro.dev>
2020-08-08 06:49:34 +0900

    component間で連携 179

--- understanding-ts/src/App.tsx
+++ understanding-ts/src/App.tsx
@@ -13,9 +13,12 @@ const App: React.FC = () => {
     const todos = [
         { id: 't1', text: 'TypeScriptコースの完了' },
     ];
+    const todoAddHandler = (text: string) => {
+        console.log(text);
+    };
     return (
         <div className="App">
-            <NewTodod />
+            <NewTodod onAddTodo={todoAddHandler} />
             <TodoList items={todos} />
         </div>
     );
